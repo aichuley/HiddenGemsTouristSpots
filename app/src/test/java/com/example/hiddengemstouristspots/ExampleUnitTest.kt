d85b0c82518814706a546cb6681ae98df29fc86b
ac <anthonyceja02@gmail.com>
2022-10-13 20:39:28 -0500

    Anthony push

--- app/src/test/java/com/example/hiddengemstouristspots/ExampleUnitTest.kt
+++ app/src/test/java/com/example/hiddengemstouristspots/ExampleUnitTest.kt
@@ -14,4 +14,10 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+    @Test
+    fun subtraction_isCorrect() {
+        assertEquals(0, 2 - 2)
+    }
+
+    //Anthony's Commit
 }
