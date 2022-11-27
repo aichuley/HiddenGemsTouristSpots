15f92556707a7dc951fcc23bc77572c92cd94ad8
Aichuley <ashsupp@gmail.com>
2022-11-27 17:55:06 -0600

    Added in more tourist spots from different cities for testing filtering and adding rest of french translations - Ashley

--- app/src/main/java/com/example/hiddengemstouristspots/data/SpotRoomDatabase.kt
+++ app/src/main/java/com/example/hiddengemstouristspots/data/SpotRoomDatabase.kt
@@ -95,6 +95,61 @@ public abstract class SpotRoomDatabase : RoomDatabase() {
                 "Austin"
             )
             spotDao.insert(spot)
+            spot = Spot(
+                //image from: https://www.coolworks.com/grand-canyon-south-rim/profile
+                R.drawable.grandcanyon,
+                "",
+                "Grand Canyon",
+                "The Grand Canyon is a steep-sided canyon...",
+                "4.5/5",
+                "Grand Canyon National Park, in Arizona, is home to much of the immense Grand Canyon, with its layered bands of red rock revealing millions of years of geological history."
+                , "Arizona"
+            )
+            spotDao.insert(spot)
+            spot = Spot(
+                //image from: https://www.planetware.com/tourist-attractions-/wyoming-yellowstone-national-park-us-wy-yellow.htm
+                R.drawable.usa_wyoming_yellowstone_grand_prismatic_spring,
+                "",
+                "Yellow Stone National Park",
+                "Yellowstone National Park is a nearly 3,500-sq.-mile...",
+                "4.5/5",
+                "Mostly in Wyoming, the park spreads into parts of Montana and Idaho too. Yellowstone features dramatic canyons, alpine rivers, lush forests, hot springs and gushing geysers, including its most famous, Old Faithful."
+                , "Wyoming"
+            )
+            spotDao.insert(spot)
+            spot = Spot(
+                //image from: https://www.tripsavvy.com/thmb/X1noqpPb0L654YUCcthg9E1Fdy4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-1164998882-6bcd8aa5b6124528b16b56089f4bf47d.jpg
+                R.drawable.sanantonio,
+                "",
+                "San Antonio River Walk",
+                "The San Antonio River Walk is a city park...",
+                "4.5/5",
+                "The River Walk winds and loops under bridges as two parallel sidewalks lined with restaurants and shops, connecting the major tourist draws such as the Shops at Rivercenter."
+                , "San Antonio"
+            )
+            spotDao.insert(spot)
+            spot = Spot(
+                //image from: https://www.history.com/topics/landmarks/golden-gate-bridge
+                R.drawable.topic_golden_gate_bridge_gettyimages_177770941,
+                "",
+                "Golden Gate Bridge",
+                "The Golden Gate Bridge is a suspension bridge...",
+                "4.8/5",
+                "The structure links the U.S. city of San Francisco, California—the northern tip of the San Francisco Peninsula—to Marin County, carrying both U.S. Route 101 and California State Route 1 across the strait."
+                , "San Francisco"
+            )
+            spotDao.insert(spot)
+            spot = Spot(
+                //image from: https://www.expressnews.com/news/local/article/Gorillas-returning-to-San-Antonio-Zoo-after-16846222.php
+                R.drawable.zoo,
+                "",
+                "San Antonio Zoo",
+                "Recently named the Best Zoo in Texas...",
+                "4.3/5",
+                "The San Antonio Zoo is an Association of Zoos and Aquariums-accredited zoo in Midtown San Antonio, Texas, United States. It is located in the city's Brackenridge Park."
+                , "San Antonio"
+            )
+            spotDao.insert(spot)
         }
     }
 }
